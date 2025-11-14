cd5643079cd1ebf3ab0df5e06d22e7a7451e372b
sushi\suresh <suresh.draco@gmail.com>
2025-11-14 15:11:36 +0100

    login feature complete

--- app/src/main/java/se/test/githubnoobtutorial/Login.java
+++ app/src/main/java/se/test/githubnoobtutorial/Login.java
@@ -1,4 +1,17 @@
 package se.test.githubnoobtutorial;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 public class Login {
+    @NonNull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        return super.equals(obj);
+    }
 }
